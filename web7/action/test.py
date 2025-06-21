016ac3f3b30ed9ee0417f192a7dad229463e145a
arjun-banerjee <abaner@berkeley.edu>
2025-06-21 14:39:59 -0700

    Agent intialziation and inital step gen

---
+++ web7/action/test.py
@@ -0,0 +1,49 @@
+from letta_client import Letta
+import ast
+
+
+client = Letta(token="sk-let-ZWUwMzAxNDYtYmFkZC00ZTA1LWJlMmUtMDM1ZmI3NTJjNGIyOjcwYjFlNDQ4LWU3MzItNGY2Ny04MjY4LTY2ZDVkMDU1YTA5NA==")
+
+def intialize_agent(tools_list = ["web_search", "run_code"]):
+    agent_state = client.agents.create(
+    model="openai/gpt-4.1",
+    embedding="openai/text-embedding-3-small",
+    memory_blocks=[
+                    {
+                    "label": "human",
+                    "value": "The human's name is Chad. They are diligent task doer that thinks deeply."
+                    },
+                    {
+                    "label": "persona",
+                    "value": "My name is Sam, the all-knowing sentient AI."
+                    }
+                ],
+                    tools=tools_list
+                )
+    return agent_state
+
+
+def generate_task_list(agent, user_input):
+    task_list_response = client.agents.messages.create(
+                agent_id=agent.id,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": f'Generate a list of tasks you need to do to acomplish: {user_input} Return the answer ONLY as a Python list of strings.'
+                    }
+                ]
+                )
+    task_list = task_list_response.messages[-1].content
+    block = client.blocks.create(
+            label="tast list",
+            description="A block to store the set of tasks that you will need to accomplish",
+            value=task_list,
+            limit=4000,
+        )
+    client.agents.blocks.attach(agent_id=agent.id, block_id=block.id)
+    return ast.literal_eval(task_list)
+
+
+agent = intialize_agent()
+task_list = generate_task_list(agent, "I want to order food")
+print(task_list)
