977b5145b1e2c66f363544178c2ba95cfe811f21
arjun-banerjee <abaner@berkeley.edu>
2025-06-21 17:37:54 -0700

    Updating entry poitns

--- web7/action/entrypoint.py
+++ web7/action/entrypoint.py
@@ -21,7 +21,7 @@ client = Letta(token=os.getenv("LETTA_API_KEY"))
 # Use the "everything" mcp server:
 # https://github.com/modelcontextprotocol/servers/tree/main/src/everything
 mcp_server_name = "web7_notion"
-mcp_tool_name = "echo"
+mcp_tool_name = "add"
 
 # List all McpTool belonging to the "everything" mcp server.
 mcp_tools = client.tools.list_mcp_tools_by_server(
@@ -31,17 +31,17 @@ for tool in mcp_tools:
     pprint(tool)
     print()
 
-# mcp_tool = client.tools.add_mcp_tool(
-#     mcp_server_name=mcp_server_name, mcp_tool_name=mcp_tool_name
-# )
-#
-#
-# # Ask the agent to call the tool.
-# response = client.agents.messages.create(
-#     agent_id="agent-eba622c8-4ab9-49db-8c66-20e83f4fb4f7",
-#     messages=[
-#         {"role": "user", "content": "Hello can you echo back this input?"},
-#     ],
-# )
-# for message in response.messages:
-#     print(message)
+mcp_tool = client.tools.add_mcp_tool(
+    mcp_server_name=mcp_server_name, mcp_tool_name=mcp_tool_name
+)
+
+
+# Ask the agent to call the tool.
+response = client.agents.messages.create(
+    agent_id="agent-eba622c8-4ab9-49db-8c66-20e83f4fb4f7",
+    messages=[
+        {"role": "user", "content": "Hello can you echo back this input?"},
+    ],
+)
+for message in response.messages:
+    print(message)
